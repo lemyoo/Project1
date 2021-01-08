3ece459f88699fd4eadb269f4a8f1c3c4bb1770b
Emmanuel Lartey mohammed <emmanuellarteymohammed@gmail.com>
2021-01-08 10:50:22 +0000

    center text

--- src/view/Entry.js
+++ src/view/Entry.js
@@ -5,7 +5,7 @@ import './styles.css'
 function Entry() {
     return (
         <div>
-            <div className="message">Use a tablet of larger screen</div>
+            <div className="message ">Use a minimum screen of 541px</div>
             <MainScreen />
         </div>
     )
